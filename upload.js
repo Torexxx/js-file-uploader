cfbaa2361d1c8e784de9df80465620efac7e96c0
torexxx <molento-studio@yandex.ru>
2021-02-24 14:23:39 +0700

    added uploaded files preview

--- upload.js
+++ upload.js
@@ -1,5 +1,18 @@
+function bytesToSize(bytes) {
+    const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
+    if (!bytes) {
+        return '0 Byte';
+    }
+    const i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)));
+    return Math.round(bytes / Math.pow(1024, i)) + ' ' + sizes[i];
+}
+
 export const upload = (selector, options) => {
+    let files = [];
     const input = document.querySelector(selector);
+    const preview = document.createElement('div');
+
+    preview.classList.add('preview')
 
     const open = document.createElement('button');
     open.classList.add('btn');
@@ -13,14 +26,64 @@ export const upload = (selector, options) => {
         input.setAttribute('accept', options.accept.join(','));
     }
 
+    input.insertAdjacentElement('afterend', preview);
     input.insertAdjacentElement('afterend', open);
 
     const triggerInput = () => input.click();
 
     const changeHandler = (e) => {
-        const file = e.target.files[0].name;
+
+        if (!e.target.files.length) {
+            return
+        }
+
+        files = Array.from(e.target.files);
+
+        preview.innerHTML = '';
+        files.forEach(file => {
+            if (!file.type.match('image')) {
+                 return
+            }
+
+            const reader = new FileReader();
+
+            reader.onload = (ev) => {
+                const src = ev.target.result
+                preview.insertAdjacentHTML('afterbegin', `
+                    <div class="preview">
+                        <div class="preview-image">
+                        <div class="preview-remove" data-name="${file.name}">&times;</div>
+                            <img src="${src}" alt="${file.name}"/>
+                            <div class="preview-info">
+                            <span>${file.name}</span> 
+                            ${bytesToSize(file.size)}
+                            </div>
+                        </div>
+                    </div>
+                `)
+            }
+
+            reader.readAsDataURL(file);
+        })
+    }
+
+    const removeHandler = (e) => {
+        if (!e.target.dataset.name) {
+            return
+        }
+
+        const {name} = e.target.dataset;
+        files = files.filter(file => file.name !== name);
+
+        const block = preview
+            .querySelector(`[data-name="${name}"]`)
+            .closest('.preview-image');
+
+        block.classList.add('removing');
+        setTimeout(() => block.remove(), 300);
     }
 
     open.addEventListener('click', triggerInput);
     input.addEventListener('change', changeHandler);
+    preview.addEventListener('click', removeHandler);
 }
